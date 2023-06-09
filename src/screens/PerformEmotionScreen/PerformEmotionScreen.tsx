7765d1ea8d67e0a21f8c952a2f9619c5e4eac311
Adrian Estevez <42591423+estevezadrian95@users.noreply.github.com>
2023-06-09 16:48:42 -0300

    clean code to API emotion

--- src/screens/PerformEmotionScreen/PerformEmotionScreen.tsx
+++ src/screens/PerformEmotionScreen/PerformEmotionScreen.tsx
@@ -1,87 +1,25 @@
-import React, {FC, useCallback, useEffect, useRef, useState} from 'react';
-import {View} from 'react-native';
-import {Text} from 'react-native-magnus';
-import {makeRuletaScreenStyles} from './PerformEmotionScreen.style';
-import {CameraComponent} from '../../components/Camera';
-import {Camera} from 'react-native-vision-camera';
-import {useAuthorizedCamera} from '../../components/Camera/useAuthorizedCamera';
-import {PerformEmotionType} from '../../stacks/HomeParams';
+import React, { FC, useCallback, useEffect, useRef, useState } from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-magnus';
+import { makeRuletaScreenStyles } from './PerformEmotionScreen.style';
+import { CameraComponent } from '../../components/Camera';
+import { Camera } from 'react-native-vision-camera';
+import { useAuthorizedCamera } from '../../components/Camera/useAuthorizedCamera';
+import { PerformEmotionType } from '../../stacks/HomeParams';
 import RNFS from 'react-native-fs';
 
-const PerformEmotionScreen: FC<PerformEmotionType> = ({route}) => {
-  const {emotion} = route.params;
+const PerformEmotionScreen: FC<PerformEmotionType> = ({ route }) => {
+  const { emotion } = route.params;
   const style = makeRuletaScreenStyles();
   const cameraRef = useRef<Camera>(null);
-  const {isAuthorized, requestCameraPermission} = useAuthorizedCamera();
+  const { isAuthorized, requestCameraPermission } = useAuthorizedCamera();
   const [imageBase64, setImageBase64] = useState('');
-  const [loading, setloading] = useState(true);
+  const [refresh, setRefresh] = useState(true);
   const [response, setResponse] = useState(Object);
   const [isInitialized, setIsInitialized] = useState<boolean>(false);
 
-  // useEffect(() => {
-  //   console.log("starting");
-  //   // TODO -- NO carga al inicio, hay que hacer una carga manual para que arranque
-  //   takePicture();
-  // }, []);
-
-  // useEffect(() => {
-  //   takePicture();
-  // }, [loading]);
-
-  useEffect(() => {
-    if (imageBase64 && imageBase64 !== '') {
-      console.log('Imagen Obtenida');
-      detectEmotionsApi(imageBase64).then(predict => {
-        const parsedJson = JSON.parse(JSON.stringify(predict));
-        const {emotions} = parsedJson;
-        console.log(emotions);
-        setResponse(emotions);
-
-        setloading(!loading);
-      });
-    }
-  }, [imageBase64, loading]);
-
-  const takePicture = useCallback(async () => {
-    try {
-      if (isAuthorized && cameraRef.current) {
-        const image = await cameraRef.current.takePhoto();
-        if (image && image.path) {
-          const base64 = await RNFS.readFile(image.path, 'base64');
-          setImageBase64(base64);
-        } else {
-          console.error('Error al capturar la imagen');
-        }
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  }, [isAuthorized]);
-
-  // const takePicture = async () => {
-  //   try {
-  //     if (isAuthorized && cameraRef.current) {
-  //       const image = await cameraRef.current.takePhoto();
-  //       if (image && image.path) {
-  //         const base64 = await RNFS.readFile(image.path, 'base64');
-  //         setImageBase64(base64);
-  //       } else {
-  //         console.error('Error al capturar la imagen');
-  //       }
-  //     }
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-  // };
-
-  useEffect(() => {
-    if (isInitialized) {
-      takePicture();
-    }
-  }, [isInitialized, takePicture]);
-
   const detectEmotionsApi = (imageData: string) => {
-    const url = 'http://192.168.0.21:3001/detect-emotion';
+    const url = 'http://192.168.0.99:3001/detect-emotion';
     const body = {
       image: imageData,
     };
@@ -103,6 +41,45 @@ const PerformEmotionScreen: FC<PerformEmotionType> = ({route}) => {
         });
     });
   };
+
+  const takePicture = useCallback(async () => {
+    try {
+      if (isAuthorized && cameraRef.current) {
+        const image = await cameraRef.current.takePhoto();
+        if (image && image.path) {
+          const base64 = await RNFS.readFile(image.path, 'base64');
+          setImageBase64(base64);
+        } else {
+          console.error('Error al capturar la imagen');
+        }
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }, [isAuthorized]);
+
+  useEffect(() => {
+    if (imageBase64 && imageBase64 !== '') {
+      console.log('Imagen Obtenida');
+      detectEmotionsApi(imageBase64).then(predict => {
+        const parsedJson = JSON.parse(JSON.stringify(predict));
+        const { emotions } = parsedJson;
+        console.log(emotions);
+        setResponse(emotions);
+        setRefresh(!refresh);
+      });
+    }
+  }, [imageBase64]);
+
+  useEffect(() => {
+    if (isInitialized) {
+      takePicture();
+    }
+  }, [isInitialized, takePicture]);
+
+  useEffect(() => {
+    takePicture();
+  }, [refresh]);
 
   return (
     <View style={style.containerView}>
