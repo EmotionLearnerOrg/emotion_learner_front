04ce078930c0de7db076be124cdc2f75c122d86f
JulianVies <julianviescas@gmail.com>
2023-07-26 13:40:44 -0300

    creada pantalla inicial de asociacion y agregadas imagenes de monstruos y opciones

--- src/components/Ruleta/emociones.ts
+++ src/components/Ruleta/emociones.ts
@@ -1,16 +1,3 @@
-import {
-  FelizGuia,
-  FelizReal,
-  TristeGuia,
-  TristeReal,
-  SorpresaGuia,
-  SorpresaReal,
-  EnojadoGuia,
-  EnojadoReal,
-  NeutralGuia,
-  NeutralReal,
-} from '../../../assets';
-
 export enum emocionesEnum {
   FELIZ = 'feliz',
   TRISTE = 'triste',
@@ -33,60 +20,36 @@ export const emociones: {[K in emocionesEnum]: emocionType} = {
   [emocionesEnum.FELIZ]: {
     color: '#FDFF49',
     name: emocionesEnum.FELIZ,
-    pathGuia: FelizGuia,
-    pathReal: FelizReal,
+    pathGuia: require('./../../../assets/ilustraciones/feliz/instructivo/guia-feliz.png'),
+    pathReal: require('./../../../assets/ilustraciones/feliz/instructivo/guia-real.png'),
     pathMonstruo: require('./../../../assets/ilustraciones/feliz/monstruo_feliz.png'),
   },
   [emocionesEnum.TRISTE]: {
     color: '#65ADFC',
     name: emocionesEnum.TRISTE,
-    pathGuia: TristeGuia,
-    pathReal: TristeReal,
+    pathGuia: require('./../../../assets/ilustraciones/triste/instructivo/guia-triste.png'),
+    pathReal: require('./../../../assets/ilustraciones/triste/instructivo/guia-real.png'),
     pathMonstruo: require('./../../../assets/ilustraciones/triste/monstruo_triste.png'),
   },
   [emocionesEnum.SORPRENDIDO]: {
     color: '#BBBBBB',
     name: emocionesEnum.SORPRENDIDO,
-    pathGuia: SorpresaGuia,
-    pathReal: SorpresaReal,
+    pathGuia: require('./../../../assets/ilustraciones/sorpresa/instructivo/guia-sorpresa.png'),
+    pathReal: require('./../../../assets/ilustraciones/sorpresa/instructivo/guia-real.png'),
     pathMonstruo: require('./../../../assets/ilustraciones/sorpresa/monstruo_sorprendido.png'),
   },
   [emocionesEnum.ENOJADO]: {
     color: '#FC4D4A',
     name: emocionesEnum.ENOJADO,
-    pathGuia: EnojadoGuia,
-    pathReal: EnojadoReal,
+    pathGuia: require('./../../../assets/ilustraciones/enojado/instructivo/guia-enojado.png'),
+    pathReal: require('./../../../assets/ilustraciones/enojado/instructivo/guia-real.png'),
     pathMonstruo: require('./../../../assets/ilustraciones/enojado/monstruo_enojado.png'),
   },
   [emocionesEnum.NEUTRAL]: {
     color: '#88EA5A',
     name: emocionesEnum.NEUTRAL,
-    pathGuia: NeutralGuia,
-    pathReal: NeutralReal,
+    pathGuia: require('./../../../assets/ilustraciones/neutral/instructivo/guia-neutral.png'),
+    pathReal: require('./../../../assets/ilustraciones/neutral/instructivo/guia-real.png'),
     pathMonstruo: require('./../../../assets/ilustraciones/neutral/monstruo_neutral.png'),
   },
-};
-
-const blabla = {
-  arcade: [
-    ['arcade_feliz', false],
-    ['arcade_triste', false],
-    ['arcade_sorprendido', false],
-    ['arcade_enojado', false],
-    ['arcade_neutral', false],
-  ],
-  mirror: [
-    ['mirror_feliz', false],
-    ['mirror_triste', false],
-    ['mirror_sorprendido', false],
-    ['mirror_enojado', false],
-    ['mirror_neutral', false],
-  ],
-  ruleta: [
-    ['ruleta_feliz', false],
-    ['ruleta_triste', false],
-    ['ruleta_sorprendido', false],
-    ['ruleta_enojado', false],
-    ['ruleta_neutral', false],
-  ],
 };
--- src/screens/AsociationScreen/AsociationScreen.tsx
+++ src/screens/AsociationScreen/AsociationScreen.tsx
@@ -1,14 +1,53 @@
 import React, {FC} from 'react';
-import {ScrollView} from 'react-native';
+import {Image, View, TouchableOpacity} from 'react-native';
 import {makeAsociationScreenStyles} from './AsociationScreen.style';
-import {Text} from 'react-native-magnus';
+import {Button, Text} from 'react-native-magnus';
+import {HomeRoutes, AsociationType} from '../../stacks/HomeParams';
+import {emocionType, emociones} from '../../components/Ruleta/emociones';
 
-const AsociationScreen: FC<{}> = () => {
+const AsociationScreen: FC<AsociationType> = ({navigation}) => {
+  const emotions = Object.values(emociones);
+  var RandomNumber = Math.floor(Math.random() * 5) ;
   const style = makeAsociationScreenStyles();
+
+  function reload() {
+    RandomNumber = Math.floor(Math.random() * 5) ;
+    console.log(RandomNumber);
+    navigation.navigate(HomeRoutes.ASOCIATION)
+  }
+
   return (
-    <ScrollView style={style.containerView}>
-      <Text>Asociation</Text>
-    </ScrollView>
+    <View style={[style.containerView, {flexDirection: 'column', alignItems:'center'}]}>
+      <Image source={emotions[RandomNumber].pathMonstruo} style={{ width: 120, resizeMode: 'contain', flex: 1}} />
+
+      <Text fontSize={18} textAlign="center" style={{flex:1}}>
+        ¿Cual de estos personajes está {emotions[RandomNumber].name}?
+      </Text>
+
+      <View style={{flex: 1, flexDirection: 'row', alignItems:'flex-start', justifyContent : 'space-evenly'}}>
+        
+        <TouchableOpacity style={{flex: 1}} activeOpacity={0.5} onPress={() => reload()}>
+            <Image 
+              source={require('./../../../assets/ilustraciones/opciones/1.png')}
+              style={style.OptionImageStyle}
+            />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={{flex: 1}} activeOpacity={0.5} onPress={() => navigation.navigate(HomeRoutes.ASOCIATION)}>
+            <Image 
+              source={require('./../../../assets/ilustraciones/opciones/2.png')}
+              style={style.OptionImageStyle}
+            />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={{flex: 1}} activeOpacity={0.5} onPress={() => navigation.navigate(HomeRoutes.ASOCIATION)}>
+            <Image 
+              source={require('./../../../assets/ilustraciones/opciones/3.png')}
+              style={style.OptionImageStyle}
+            />
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
