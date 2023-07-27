4c053df00a76a154e44cec7061403468d145d0e6
JulianVies <julianviescas@gmail.com>
2023-07-26 22:51:28 -0300

    agregado aleatorio para opciones

--- src/screens/AsociationScreen/AsociationScreen.tsx
+++ src/screens/AsociationScreen/AsociationScreen.tsx
@@ -7,42 +7,65 @@ import {emocionType, emociones} from '../../components/RuletaContainer/emociones
 
 const AsociationScreen: FC<AsociationType> = ({navigation}) => {
   const emotions = Object.values(emociones);
-  var RandomNumber = Math.floor(Math.random() * 5) ;
+  var RandomNumber1 = Math.floor(Math.random() * 5) ;
+  var RandomNumber2 = Math.floor(Math.random() * 5) ;
+  var RandomNumber3 = Math.floor(Math.random() * 5) ;
   const style = makeAsociationScreenStyles();
 
+  const opcion1 = getOptionImage(RandomNumber1);
+  const opcion2 = getOptionImage(RandomNumber2);
+  const opcion3 = getOptionImage(RandomNumber3);
+
   function reload() {
-    RandomNumber = Math.floor(Math.random() * 5) ;
-    console.log(RandomNumber);
     navigation.navigate(HomeRoutes.ASOCIATION)
+  }
+
+  function getOptionImage(id) {
+    if (id==0) {
+      return require('./../../../assets/ilustraciones/feliz/opciones/1.png');
+    }
+    if (id==1) {
+      return require('./../../../assets/ilustraciones/triste/opciones/1.png');
+    }
+    if (id==2) {
+      return require('./../../../assets/ilustraciones/sorpresa/opciones/1.png');
+    }
+    if (id==3) {
+      return require('./../../../assets/ilustraciones/enojado/opciones/1.png');
+    }
+    if (id==4) {
+      return require('./../../../assets/ilustraciones/neutral/opciones/1.png');
+    }
+    return "";
   }
 
   return (
     <View style={[style.containerView, {flexDirection: 'column', alignItems:'center'}]}>
-      <Image source={emotions[RandomNumber].pathMonstruo} style={{ width: 120, resizeMode: 'contain', flex: 1}} />
+      <Image source={emotions[RandomNumber1].pathMonstruo} style={{ width: 120, resizeMode: 'contain', flex: 1}} />
 
       <Text fontSize={18} textAlign="center" style={{flex:1}}>
-        ¿Cual de estos personajes está {emotions[RandomNumber].name}?
+        ¿Cual de estos personajes está {emotions[RandomNumber1].name}?
       </Text>
 
       <View style={{flex: 1, flexDirection: 'row', alignItems:'flex-start', justifyContent : 'space-evenly'}}>
         
         <TouchableOpacity style={{flex: 1}} activeOpacity={0.5} onPress={() => reload()}>
             <Image 
-              source={require('./../../../assets/ilustraciones/opciones/1.png')}
+              source={opcion1}
               style={style.OptionImageStyle}
             />
         </TouchableOpacity>
 
-        <TouchableOpacity style={{flex: 1}} activeOpacity={0.5} onPress={() => navigation.navigate(HomeRoutes.ASOCIATION)}>
+        <TouchableOpacity style={{flex: 1}} activeOpacity={0.5} onPress={() => reload()}>
             <Image 
-              source={require('./../../../assets/ilustraciones/opciones/2.png')}
+              source={opcion2}
               style={style.OptionImageStyle}
             />
         </TouchableOpacity>
 
-        <TouchableOpacity style={{flex: 1}} activeOpacity={0.5} onPress={() => navigation.navigate(HomeRoutes.ASOCIATION)}>
+        <TouchableOpacity style={{flex: 1}} activeOpacity={0.5} onPress={() => reload()}>
             <Image 
-              source={require('./../../../assets/ilustraciones/opciones/3.png')}
+              source={opcion3}
               style={style.OptionImageStyle}
             />
         </TouchableOpacity>
